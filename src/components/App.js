910016cf751277b7ce080fd843b0188db16dc68f
NilsonNetto <eng.nilsonnetto@gmail.com>
2022-09-14 06:27:48 -0300

    feat: Login page and register page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
-import styled from "styled-components";
+import GlobalStyle from "../assets/GlobalStyle.js";
+import LoginPage from "./LoginPage.js";
+import UserContext from "../contexts/UserContext.js";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterPage from "./RegisterPage.js";
 
 export default function App() {
+
+  const [userData, setUserData] = useState([]);
+
   return (
-    <Teste>Teste</Teste>
+    <UserContext.Provider value={{ userData, setUserData }}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
-
-const Teste = styled.div`
-  width: 100%;
-  background-color: red;
-`;
